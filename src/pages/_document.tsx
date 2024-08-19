31460cbf86749165465766136ae538f1828cb41a
IggyPope <igor.a.vakulchik@gmail.com>
2024-08-19 23:09:15 +0300

    chore: configure eslint and prettier

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 bg-white`}
+      className={`bg-white flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <p>Home page</p>
     </main>
