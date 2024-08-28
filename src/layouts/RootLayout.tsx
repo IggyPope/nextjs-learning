f7f2551f4b0b9457640ceff1be53e2254b2e013f
IggyPope <igor.a.vakulchik@gmail.com>
2024-08-29 02:46:43 +0300

    fix: fix responsiveness of the row-gap between the header, main and footer

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,14 @@
+import classNames from 'classnames';
 import { Footer } from '@/components/common/footer/Footer';
 import { Header } from '@/components/common/header/Header';
 
 export const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div className="flex h-full min-h-screen flex-col gap-10">
+  <div
+    className={classNames(
+      'flex h-full min-h-screen flex-col gap-5',
+      'md:gap-10',
+    )}
+  >
     <Header />
     {children}
     <Footer />
